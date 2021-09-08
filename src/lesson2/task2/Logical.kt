febdc43e7c9fc95ff93fab110a9c780b4aa4035c
rinpatch <rin@patch.cx>
2021-09-08 15:24:10 +0300

    Solved lesson 2 task 2

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -3,6 +3,8 @@
 package lesson2.task2
 
 import lesson1.task1.sqr
+import lesson1.task1.trackLength
+import kotlin.math.abs
 
 /**
  * Пример
@@ -18,7 +20,11 @@ fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
  * Четырехзначное число назовем счастливым, если сумма первых двух ее цифр равна сумме двух последних.
  * Определить, счастливое ли заданное число, вернуть true, если это так.
  */
-fun isNumberHappy(number: Int): Boolean = TODO()
+fun isNumberHappy(number: Int): Boolean {
+    val firstSum = number / 1000 + number / 100 % 10
+    val secondSum = number % 100 / 10 + number % 10
+    return firstSum == secondSum
+}
 
 /**
  * Простая (2 балла)
@@ -27,7 +33,9 @@ fun isNumberHappy(number: Int): Boolean = TODO()
  * Определить, угрожают ли они друг другу. Вернуть true, если угрожают.
  * Считать, что ферзи не могут загораживать друг друга.
  */
-fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
+fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean {
+    return x1 == x2 || y1 == y2 || abs(x1 - x2) == abs(y1 - y2)
+}
 
 
 /**
@@ -36,7 +44,14 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
  * Дан номер месяца (от 1 до 12 включительно) и год (положительный).
  * Вернуть число дней в этом месяце этого года по григорианскому календарю.
  */
-fun daysInMonth(month: Int, year: Int): Int = TODO()
+fun daysInMonth(month: Int, year: Int): Int {
+    val leapYear = year % 400 == 0 || (year % 100 != 0 && year % 4 == 0)
+    return when (month) {
+        2 -> if (leapYear) 29 else 28
+        1, 3, 5, 7, 8, 10, 12 -> 31
+        else -> 30
+    }
+}
 
 /**
  * Простая (2 балла)
@@ -48,7 +63,10 @@ fun daysInMonth(month: Int, year: Int): Int = TODO()
 fun circleInside(
     x1: Double, y1: Double, r1: Double,
     x2: Double, y2: Double, r2: Double
-): Boolean = TODO()
+): Boolean {
+    val distance = trackLength(x1, y1, x2, y2)
+    return distance + r1 <= r2
+}
 
 /**
  * Средняя (3 балла)
