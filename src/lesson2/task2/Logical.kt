328b78a71f53ee7d90670b6592531fbe8d0e20c5
rinpatch <rin@patch.cx>
2021-09-10 16:32:00 +0300

    Use expression body for queenThreatens

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -33,9 +33,8 @@ fun isNumberHappy(number: Int): Boolean {
  * Определить, угрожают ли они друг другу. Вернуть true, если угрожают.
  * Считать, что ферзи не могут загораживать друг друга.
  */
-fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean {
-    return x1 == x2 || y1 == y2 || abs(x1 - x2) == abs(y1 - y2)
-}
+fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean =
+    x1 == x2 || y1 == y2 || abs(x1 - x2) == abs(y1 - y2)
 
 
 /**
