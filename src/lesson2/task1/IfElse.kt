81de1aa6598a248d189227ff9a0d7ea0a703c9f7
ДмитрийТ <tuchov.da@edu.spbstu.ru>
2021-09-11 16:32:29 +0300

    лессон 2 таск 1

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -3,8 +3,7 @@
 package lesson2.task1
 
 import lesson1.task1.discriminant
-import kotlin.math.max
-import kotlin.math.sqrt
+import kotlin.math.*
 
 // Урок 2: ветвления (здесь), логический тип (см. 2.2).
 // Максимальное количество баллов = 6
@@ -68,8 +67,13 @@ fun minBiRoot(a: Double, b: Double, c: Double): Double {
  * Мой возраст. Для заданного 0 < n < 200, рассматриваемого как возраст человека,
  * вернуть строку вида: «21 год», «32 года», «12 лет».
  */
-fun ageDescription(age: Int): String = TODO()
-
+fun ageDescription(age: Int): String {
+    if ((age % 10 == 0) || ((age >= 5) && (age < 20))) {
+        return ("$age лет");
+    } else if (age % 10 == 1) {
+        return("$age год");
+    } else {return("$age года");}
+}
 /**
  * Простая (2 балла)
  *
@@ -81,55 +85,135 @@ fun timeForHalfWay(
     t1: Double, v1: Double,
     t2: Double, v2: Double,
     t3: Double, v3: Double
-): Double = TODO()
+): Double {
+    val time: Array<Double> = arrayOf(t1, t2, t3);
+    val speed: Array<Double> = arrayOf(v1, v2, v3);
+    var Shalf:Double =(t1*v1+t2*v2+t3*v3)/2;
+    var Thalf:Double = 0.0;
 
-/**
- * Простая (2 балла)
- *
- * Нa шахматной доске стоят черный король и две белые ладьи (ладья бьет по горизонтали и вертикали).
- * Определить, не находится ли король под боем, а если есть угроза, то от кого именно.
- * Вернуть 0, если угрозы нет, 1, если угроза только от первой ладьи, 2, если только от второй ладьи,
- * и 3, если угроза от обеих ладей.
- * Считать, что ладьи не могут загораживать друг друга
- */
-fun whichRookThreatens(
-    kingX: Int, kingY: Int,
-    rookX1: Int, rookY1: Int,
-    rookX2: Int, rookY2: Int
-): Int = TODO()
+    for (t in time)
+    {
+        for (v in speed )
+        {
+            if ((Shalf/v)>t)
+            {
+                Shalf = Shalf - (v*t);
+                Thalf = Thalf + t;
+            }
+            else
+            {
+                Thalf=Thalf+(Shalf/v);
+                break
+            }
+        }
+    }
+    return Thalf
+}
 
-/**
- * Простая (2 балла)
- *
- * На шахматной доске стоят черный король и белые ладья и слон
- * (ладья бьет по горизонтали и вертикали, слон — по диагоналям).
- * Проверить, есть ли угроза королю и если есть, то от кого именно.
- * Вернуть 0, если угрозы нет, 1, если угроза только от ладьи, 2, если только от слона,
- * и 3, если угроза есть и от ладьи и от слона.
- * Считать, что ладья и слон не могут загораживать друг друга.
- */
-fun rookOrBishopThreatens(
-    kingX: Int, kingY: Int,
-    rookX: Int, rookY: Int,
-    bishopX: Int, bishopY: Int
-): Int = TODO()
+    /**
+     * Простая (2 балла)
+     *
+     * Нa шахматной доске стоят черный король и две белые ладьи (ладья бьет по горизонтали и вертикали).
+     * Определить, не находится ли король под боем, а если есть угроза, то от кого именно.
+     * Вернуть 0, если угрозы нет, 1, если угроза только от первой ладьи, 2, если только от второй ладьи,
+     * и 3, если угроза от обеих ладей.
+     * Считать, что ладьи не могут загораживать друг друга
+     */
+    fun whichRookThreatens(
+        kingX: Int, kingY: Int,
+        rookX1: Int, rookY1: Int,
+        rookX2: Int, rookY2: Int
+    ): Int {
+        if((kingX==rookX1)|| (rookY1==kingY))
+        {
+            if((kingX==rookX2)|| (rookY2==kingY))
+            {
+                return 3;
+            } else
+            {
+                return 1;
+            }
+        }
+        else if((kingX==rookX2)|| (rookY2==kingY))
+        {
+            return 2;
+        }
+        else
+        {
+            return 0;
+        }
+    }
 
-/**
- * Простая (2 балла)
- *
- * Треугольник задан длинами своих сторон a, b, c.
- * Проверить, является ли данный треугольник остроугольным (вернуть 0),
- * прямоугольным (вернуть 1) или тупоугольным (вернуть 2).
- * Если такой треугольник не существует, вернуть -1.
- */
-fun triangleKind(a: Double, b: Double, c: Double): Int = TODO()
 
-/**
- * Средняя (3 балла)
- *
- * Даны четыре точки на одной прямой: A, B, C и D.
- * Координаты точек a, b, c, d соответственно, b >= a, d >= c.
- * Найти длину пересечения отрезков AB и CD.
- * Если пересечения нет, вернуть -1.
- */
-fun segmentLength(a: Int, b: Int, c: Int, d: Int): Int = TODO()
+
+    /**
+     * Простая (2 балла)
+     *
+     * На шахматной доске стоят черный король и белые ладья и слон
+     * (ладья бьет по горизонтали и вертикали, слон — по диагоналям).
+     * Проверить, есть ли угроза королю и если есть, то от кого именно.
+     * Вернуть 0, если угрозы нет, 1, если угроза только от ладьи, 2, если только от слона,
+     * и 3, если угроза есть и от ладьи и от слона.
+     * Считать, что ладья и слон не могут загораживать друг друга.
+     */
+    fun rookOrBishopThreatens(
+        kingX: Int, kingY: Int,
+        rookX: Int, rookY: Int,
+        bishopX: Int, bishopY: Int
+    ): Int {
+        if((kingX==rookX)|| (rookY==kingY))
+        {
+            if(abs(kingX - bishopX) == abs(kingY - bishopY))
+            {
+                return 3;
+            } else
+            {
+                return 1;
+            }
+        } else if(abs(kingX - bishopX) == abs(kingY - bishopY))
+        {
+            return 2;
+        }
+        else{
+            return 0;
+        }
+    }
+
+
+    /**
+     * Простая (2 балла)
+     *
+     * Треугольник задан длинами своих сторон a, b, c.
+     * Проверить, является ли данный треугольник остроугольным (вернуть 0),
+     * прямоугольным (вернуть 1) или тупоугольным (вернуть 2).
+     * Если такой треугольник не существует, вернуть -1.
+     */
+    fun triangleKind(a: Double, b: Double, c: Double): Int
+    {
+        if (((a*a)+(b*b))<(c*c)) return 2;
+        else if (((a*a)+(b*b))>(c*c)) return 0;
+        else if (((a*a)+(b*b))==(c*c)) return 1;
+        else return -1;
+    }
+
+    /**
+     * Средняя (3 балла)
+     *
+     * Даны четыре точки на одной прямой: A, B, C и D.
+     * Координаты точек a, b, c, d соответственно, b >= a, d >= c.
+     * Найти длину пересечения отрезков AB и CD.
+     * Если пересечения нет, вернуть -1.
+     */
+   fun segmentLength(a: Int, b: Int, c: Int, d: Int): Int
+   {
+       if ((b<c)||(d<a)) return -1;
+       else {
+           if(min(a,c)==a){
+               return b-c;
+           } else return d-a;
+
+       }
+    }
+
+
+
