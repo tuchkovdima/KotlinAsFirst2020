be4c2098db01619bf6995c009a5490096c00bf86
rinpatch <rin@patch.cx>
2021-10-14 22:15:06 +0300

    lesson3: minDivisor: reduce the interval that needs to be iterated over

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -112,7 +112,7 @@ fun fib(n: Int): Int {
  * Для заданного числа n > 1 найти минимальный делитель, превышающий 1
  */
 fun minDivisor(n: Int): Int {
-    for (i in 2..n) {
+    for (i in 2..sqrt(n.toDouble()).toInt()) {
         if (n % i == 0) return i
     }
     return n
