feb701c64dc1ba2fa73f179f174e6adbe32d7e93
ДмитрийТ <tuchov.da@edu.spbstu.ru>
2021-10-18 17:27:56 +0300

    лессон 4

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -241,7 +241,22 @@ fun decimalFromString(str: String, base: Int): Int = TODO()
  * 90 = XC, 100 = C, 400 = CD, 500 = D, 900 = CM, 1000 = M.
  * Например: 23 = XXIII, 44 = XLIV, 100 = C
  */
-fun roman(n: Int): String = TODO()
+val revert = listOf("I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M")
+val revertn = listOf(1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000)
+fun roman(n: Int): String {
+    val result = mutableListOf<String>()
+    var i = 12
+    var natural = n
+    while (natural != 0){
+        while (natural >= revertn[i]){
+            natural -= revertn[i]
+            result.add(revert[i])
+            i = 12
+        }
+        i--
+    }
+    return result.joinToString(separator = "")
+}
 
 /**
  * Очень сложная (7 баллов)
