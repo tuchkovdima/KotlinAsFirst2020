4dd28fd7141976ec0954516e7f7ead4c1952ba4d
rinpatch <rin@patch.cx>
2021-11-12 13:55:43 +0300

    lesson5: Simplify canBuildFrom

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -237,7 +237,7 @@ fun findCheapestStuff(stuff: Map<String, Pair<String, Double>>, kind: String): S
  *   canBuildFrom(listOf('a', 'b', 'o'), "baobab") -> true
  */
 fun canBuildFrom(chars: List<Char>, word: String): Boolean =
-    word.lowercase().asIterable().toSet().subtract(chars.map({ it.lowercaseChar() })) == setOf<Char>()
+    word.lowercase().toSet().subtract(chars.map({ it.lowercaseChar() })).isEmpty()
 
 /**
  * Средняя (4 балла)
