ae45c0a9006121b9e969a2d2d6545182481ee96b
rinpatch <rin@patch.cx>
2021-12-13 23:36:59 +0300

    lesson12: Implement hashSet

--- src/lesson12/task1/OpenHashSet.kt
+++ src/lesson12/task1/OpenHashSet.kt
@@ -25,28 +25,76 @@ class OpenHashSet<T>(val capacity: Int) {
     /**
      * Число элементов в хеш-таблице
      */
-    val size: Int get() = TODO()
+    val size: Int get() = elements.count({ it != null })
 
     /**
      * Признак пустоты
      */
-    fun isEmpty(): Boolean = TODO()
+    fun isEmpty(): Boolean = size == 0
+
+    private fun hash(element: T) = element.hashCode().mod(capacity)
 
     /**
      * Добавление элемента.
      * Вернуть true, если элемент был успешно добавлен,
      * или false, если такой элемент уже был в таблице, или превышена вместимость таблицы.
      */
-    fun add(element: T): Boolean = TODO()
+    fun add(element: T): Boolean {
+        val hash = hash(element)
+
+        if(size != capacity) {
+            for(i in hash until capacity) {
+                when(elements[i]) {
+                    null -> {
+                        elements[i] = element
+                        return true
+                    }
+                    element -> return false
+                }
+            }
+        }
+        return false
+    }
 
     /**
      * Проверка, входит ли заданный элемент в хеш-таблицу
      */
-    operator fun contains(element: T): Boolean = TODO()
+    operator fun contains(element: T): Boolean {
+        val hash = hash(element)
+
+        for(i in hash until capacity) {
+            if (elements[i] == element) return true
+            else if (elements[i] == null) return false
+        }
+        return false
+    }
 
     /**
      * Таблицы равны, если в них одинаковое количество элементов,
      * и любой элемент из второй таблицы входит также и в первую
      */
-    override fun equals(other: Any?): Boolean = TODO()
+    override fun equals(other: Any?): Boolean {
+        return if(this === other) true
+               else if(other !is OpenHashSet<*>) false
+               else if(other.size == 0 && size == 0) true
+               else if(other.size != size) false
+               // Horrible hack for horrible language
+               else if(elements.find({ it != null})!!::class != other.elements.find({ it != null})!!::class) false
+               else {
+                 for (i in elements) {
+                    if(i != null && (other as OpenHashSet<T>).contains(i as T) == false) return false
+                 }
+                 true
+               }
+    }
+    override fun hashCode(): Int {
+        val hashes = mutableListOf<Int>()
+        elements.filter({it != null}).forEach {hashes.add(it.hashCode())}
+        hashes.sort()
+        var hashCode = 0
+        for (hash in hashes) {
+            hashCode = 31 * hashCode + hash
+        }
+        return hashCode
+    }
 }
